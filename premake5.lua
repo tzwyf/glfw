3120c5fcaaff617180ff766c91ce30b54db8975c
LAPTOP-DIS8N0UP\谷底凡人 <1623327743@qq.com>
2024-07-14 15:27:55 +0800

    静态库和无警告

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
-	warnings "off"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -27,56 +26,8 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:macosx"
-		pic "On"
-
-		files
-		{
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_COCOA"
-		}
-
 	filter "system:windows"
-		systemversion "latest"
+		systemversion "10.0.17763.0"
 
 		files
 		{
@@ -99,17 +50,9 @@ project "GLFW"
 		}
 
 	filter "configurations:Debug"
-    defines "HZ_DEBUG"
-    buildoptions "/MTd"
-    symbols "On"
+		runtime "Debug"
+		symbols "On"
 
-    filter "configurations:Release"
-    defines "HZ_RELEASE"
-    buildoptions "/MT"
-    symbols "On"
-
-    filter "configurations:Dist"
-    defines "HZ_DIST"
-    buildoptions "/MT"
-    symbols "On"
-
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
